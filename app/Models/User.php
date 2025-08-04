d5a10ba025d0d41d9466ce84e0f3150c84b9f5a4
Xyryllium <sigh.real.xa@gmail.com>
2025-08-04 12:51:42 +0800

    Added phpstan neon file and remove expose app key and fix phpstan error

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,8 +10,8 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens;
+    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
     use Notifiable;
 
