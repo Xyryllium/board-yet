3d255d806e245480ea51bd1731857ccbd7101054
Xyryllium <sigh.real.xa@gmail.com>
2025-08-05 19:05:37 +0800

    Added logging channel for organization service

--- config/logging.php
+++ config/logging.php
@@ -127,6 +127,12 @@ return [
             'path' => storage_path('logs/laravel.log'),
         ],
 
+        'organization' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/organization.log'),
+            'level' => 'debug',
+        ],
+
     ],
 
 ];
