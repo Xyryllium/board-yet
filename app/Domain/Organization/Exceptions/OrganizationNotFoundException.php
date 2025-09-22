f649cbd3fafa8966e66be83df4e2957cc59aa367
Xyryllium <sigh.real.xa@gmail.com>
2025-09-22 12:23:18 +0800

    Fix phpcs error in exception class

--- app/Domain/Organization/Exceptions/OrganizationNotFoundException.php
+++ app/Domain/Organization/Exceptions/OrganizationNotFoundException.php
@@ -6,8 +6,11 @@ use Exception;
 
 class OrganizationNotFoundException extends Exception
 {
-    public function __construct(string $message = "Organization not found", int $code = 404, ?\Throwable $previous = null)
-    {
+    public function __construct(
+        string $message = "Organization not found",
+        int $code = 404,
+        ?\Throwable $previous = null
+    ) {
         parent::__construct($message, $code, $previous);
     }
 }
